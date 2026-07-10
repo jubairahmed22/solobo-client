8b5a0e83362675ac16ed9c6430c1c82794a53c2e
jubairahmed22 <jubairahmed060@gmail.com>
2026-07-10 12:46:36 +0600

    added something

--- components/layout/Navbar/Navbar.tsx
+++ components/layout/Navbar/Navbar.tsx
@@ -24,7 +24,7 @@ import type { CategoryTreeNode } from "@/types/catalog";
 export interface NavbarProps {
   categories?: CategoryNode[];
   brands?: BrandLite[];
-}
+}asdfasfd
 
 export function Navbar({ categories: ssrCategories, brands: ssrBrands }: NavbarProps) {
   const router = useRouter();
